1406a25545e0a36e69469e9042f3c0185259a1dd
Patrick Sodré <psodre@gmail.com>
2020-04-26 23:38:39 -0400

    Update for go 1.12.17

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,6 @@
 
 build_linux()
 {
-    CGO_LDFLAGS="-Wl,-O2 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,--disable-new-dtags -Wl,--gc-sections"
-    CGO_LDFLAGS=${CGO_LDFLAGS/-Wl,-O2/}
-    CGO_LDFLAGS=${CGO_LDFLAGS/-Wl,--sort-common/}
-    CGO_LDFLAGS=${CGO_LDFLAGS/-Wl,--as-needed/}
-    CGO_LDFLAGS=${CGO_LDFLAGS/-Wl,-z,relro/}
-    CGO_LDFLAGS=${CGO_LDFLAGS/-Wl,-z,now/}
-    CGO_LDFLAGS=${CGO_LDFLAGS/-Wl,--disable-new-dtags/}
-    CGO_LDFLAGS=${CGO_LDFLAGS/-Wl,--gc-sections/}
-
-    export CGO_LDFLAGS
     go env
 
     make all WHAT=cmd/hyperkube GCFLAGS="-v"
@@ -37,7 +27,6 @@ build_osx()
 }
 
 mkdir -p $PREFIX/bin
-pushd k8s.io/kubernetes
 export GO_TMPDIR=$SRCDIR/tmp
 
 case $(uname -s) in
