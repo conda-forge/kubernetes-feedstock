67332ab78e79cb89acf491222e7d2a49ac06465a
Patrick Sodré <psodre@gmail.com>
2020-04-27 20:05:45 -0400

    Make the LIBRARY_BIN directory

--- recipe/client/bld.bat
+++ recipe/client/bld.bat
@@ -1,1 +1,2 @@
-XCOPY node\bin\kubectl.exe %LIBRARY_BIN%
+MKDIR %LIBRARY_BIN%
+XCOPY node\bin\kubectl.exe %LIBRARY_BIN%\kubectl.exe
--- recipe/node/bld.bat
+++ recipe/node/bld.bat
@@ -1,2 +1,3 @@
+MKDIR %LIBRARY_BIN%
 XCOPY node\bin\*.exe %LIBRARY_BIN%
 RM %LIBRARY_BIN%\kubectl.exe
