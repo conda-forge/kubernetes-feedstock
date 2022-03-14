7de862d1006b99d243d228e6938f954a8b5d0115
Stepan Hruda <stepanhruda@fb.com>
2022-03-14 15:18:38 -0400

    Change recipe for macOS arm64 builds

--- recipe/client/build.sh
+++ recipe/client/build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -euf
 
+if [[ "$GOOS" == "darwin" && "$GOARCH" == "arm64" ]]; then
+  # Makefile has its own setup for target/platform
+  export KUBE_BUILD_PLATFORMS=darwin/arm64
+  # Only binaries with host arch go to bin/
+  # to replicate go install behavior
+  OUTPUT_DIR="local/bin/${KUBE_BUILD_PLATFORMS}"
+else
+  OUTPUT_DIR=bin
+fi
+
 # This comes from k8s
 . hack/lib/init.sh
 
@@ -11,5 +21,5 @@ go env
 make all WHAT="${KUBE_CLIENT_TARGETS[*]}"
 
 for cmd in ${KUBE_CLIENT_BINARIES[*]}; do
-  cp "_output/bin/${cmd}" "$PREFIX/bin"
+  cp "_output/${OUTPUT_DIR}/${cmd}" "$PREFIX/bin"
 done
